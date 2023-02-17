6d6f9178177048aa1ed9f7a9b99d4de27a3df56a
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2023-02-17 09:51:11 +0100

    created Todo.class

---
+++ Todo.class.php
@@ -0,0 +1,26 @@
+<?php   
+
+class Todo{
+    //tododb - db
+    // todo - tabell
+
+
+    //properties
+    private $db;
+    private $todo;
+    private $todoList = [];
+
+    //constructor
+    function __constructor() {
+        
+    }
+    //add todo
+
+    //get thing
+
+
+    //destructor
+}
+
+
+?>
